cef51d89e561f419d44edbe876d7042920efe677
Dhuragadda <parabrahmachary07@gmail.com>
2025-06-28 18:54:13 +0530

    Create init_database.sql

---
+++ Script/init_database.sql
@@ -0,0 +1,27 @@
+/*
+  Under Database create - 
+                        🥉"Bronze_layer", 🥈"Silver_layer", 🥇"Gold_layer"
+
+  
+⚠ WARNING:- 
+            Running this scritp will check for the existence of 'DataWarehouse' database if not found its creates new database.
+            if in case database exists sql through an ***error***.
+*/
+
+-- creating the "DataWarehouse" database in MYSQL
+CREATE DATABASE IF NOT EXISTS DataWarehouse;
+
+USE DataWarehouse;
+
+/*
+
+creating three different layer as database. They serves different purpose.
+Since  MySQL doesn’t support schemas within a single database. In MySQL, each schema is actually a database. 
+So, if you want something like "DataWarehouse_LayerName", that level of hierarchy isn’t supported directly.
+
+*/
+
+
+CREATE DATABASE IF NOT EXISTS DataWarehouse_Bronze;  
+CREATE DATABASE IF NOT EXISTS DataWarehouse_silver;  
+CREATE DATABASE IF NOT EXISTS DataWarehouse_gold;   
